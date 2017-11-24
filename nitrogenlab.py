29b0d5dc65b08075466720b02878d5cc5577c103
srydell <simonwrydell@gmail.com>
2017-11-24 10:15:11 +0100

    Added more data points and corrected the legend

--- nitrogenlab.py
+++ nitrogenlab.py
@@ -22,6 +22,9 @@ p7 = [0.6314, 1.908, 6.7545, 9.9275, 18.2114, 27.009, 0.33, 4.7158, 0.9316]
 p8 = [0.17395, 0.5668, 0.7182, 1.382, 1.84, 4.864, 10.45, 19.798]
 p9 = [.1665, .492, 0.7365, 1.2309, 1.6698, 1.7917, 5.15, 13.0758]
 p10 = [.11552, .396, .726, 1.312, 1.7934, 5.0388, 10.142, 19.9815]
+p11 = [.1771, .396, .75, 1.533, 2.4948, 4.9476, 15.1889, 20.2818]
+p12 = [.218, .426, .7695, 1.552, 2.546, 4.967, 15.283, 19.9]
+p13 = [.235, .396, .737, 1.491, 2.626, 5.252, 14.848, 20.202]
 
 # Mass difference (mdot) in kg/s * 10^-5
 m1 = [2.067, 1.733, 1.8, 1.867, 2.067, 3.2, 5, 9.467]
@@ -34,10 +37,13 @@ m7 = [1.73, 2, 4.4, 5.93, 9.93, 14.5, 1.27, 3.33, 1.47]
 m8 = [1.3, 1.667, 1.667, 2, 2.23, 3.533, 6.267, 10.8]
 m9 = [1.4, 1.5, 1.4, 1.5, 1.2, 1.6, 3.6, 4.3, 7.6]
 m10 = [1.53, 1.63, 1.67, 1.8, 2.27, 3.4, 5.93, 10.6]
+m11 = [1.3, 2.5, 1.8, 2, 3.5, 3.5, 8.6, 11.1]
+m12 = [1.6, 1.6, 1.8, 2.01, 2.47, 3.55, 8.6, 10.9]
+m13 = [.917, 1.5, 1.66, 2.166, 2.5, 3.58, 8.75, 11.1]
 
 
 # Combine mdata into one list
-ms = [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10]
+ms = [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13]
 mdot = []
 for m in ms:
 	mdot.extend(m)
@@ -46,7 +52,7 @@ for m in ms:
 mdot = [x * math.pow(10, -5) for x in mdot]
 
 # Combine pdata into one list
-ps = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+ps = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]
 pdot = []
 for p in ps:
 	pdot.extend(p)
@@ -84,7 +90,7 @@ ydata = func(xdata, H, 2, 20)
 popt, pcov = curve_fit(func, xdata, ydata)
 
 plt.scatter(error, enthalpy, c="#8eb5c6", edgecolors='b', label="Measured data", alpha=0.4, s=40)
-plt.plot(xdata, func(xdata, *popt), c="#094f6e", linewidth=2, label="Best fit")
+plt.plot(xdata, func(xdata, *popt), c="#094f6e", linewidth=2, label=r"$H \cdot e^{-\frac{2}{P}} + 20$")
 
 plt.yticks([0, 100, H, 300], [0, 100, r"$H =\\ {}$".format(H), 300])
 
@@ -98,4 +104,3 @@ plt.legend(loc=1, prop={"size": 18})
 # Save the plot
 plt.savefig('nitrogenEnthalpy.png', bbox_inches='tight')
 plt.show()
-
